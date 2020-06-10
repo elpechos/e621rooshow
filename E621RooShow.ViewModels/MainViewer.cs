9b4aeb2a13e88a9edc7119b70a156184cf501502
Matthew <mrdunn@gmail.com>
2020-06-10 22:55:21 +0930

    We now rebuild blocked URIs

--- E621RooShow.ViewModels/MainViewer.cs
+++ E621RooShow.ViewModels/MainViewer.cs
@@ -81,13 +81,13 @@ namespace E621RooShow.ViewModels
         {
             var files = dragonPorn.Posts.ToList();
             files.Shuffle();
-            foreach (var x in files.Where(x=>x.File != null && x.File.Url != null))
+            foreach (var x in files.Where(x=>x.File != null && x.File.RebuiltUrl != null))
             {
-                var url = new Uri(x.File.Url);
+                var url = new Uri(x.File.RebuiltUrl);
                 var urlInfo = new FileInfo(url.AbsolutePath);
 
                 if (allowedExtentions.Contains(urlInfo.Extension))
-                    filesToDownload.Enqueue(new FileDownloadInfo() { DownloadUrl = x.File.Url, E621Url = $"https://e621.net/post/show/{x.Id}/" });
+                    filesToDownload.Enqueue(new FileDownloadInfo() { DownloadUrl = x.File.RebuiltUrl, E621Url = $"https://e621.net/post/show/{x.Id}/" });
             }
         }
 
--- e621rooshow.Services/Post.cs
+++ e621rooshow.Services/Post.cs
@@ -22,6 +22,28 @@ namespace E621RooShow.Services
 
     public class File
     {
+        public string Md5 { get; set; }
+        public string Ext { get; set; }
         public string Url { get; set; }
+
+
+        public string RebuiltUrl
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Url))
+                    return Url;
+
+                if (Md5 != null)
+                {
+                    string baseUri = "https://static1.e621.net/data";
+                    string path1 = string.Concat(Md5.Take(2));
+                    string path2 = string.Concat(Md5.Skip(2).Take(2));
+                    return $"{baseUri}/{path1}/{path2}/{Md5}.{Ext}";
+                }
+
+                return null;
+            }
+        }
     }
 }
